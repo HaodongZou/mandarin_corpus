d7e6a37fb5cd0a19225b763fd527a5d10fdc3b52
samles <13263199088@163.com>
2020-04-18 21:19:28 +0800

    添加了韵书韵图数据的搜索功能
    支持检索ID或模糊查询字符

--- src/main/java/cn/zouhd/mandarin_corpus/controller/YunshuController.java
+++ src/main/java/cn/zouhd/mandarin_corpus/controller/YunshuController.java
@@ -1,12 +1,20 @@
 package cn.zouhd.mandarin_corpus.controller;
 
+import cn.zouhd.mandarin_corpus.entities.Yunshu;
 import cn.zouhd.mandarin_corpus.repositories.YunshuRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author zouhd
+ */
 @Controller
 public class YunshuController {
 
@@ -21,13 +29,31 @@ public class YunshuController {
     }
 
     @GetMapping("/yunshu/results")
-    public String results(Model model){
-        Object yunshuSearch = model.getAttribute("yunshuSearch");
+    public String results(@RequestParam String yunshuSearch, Model model){
+        StringUtils.trimAllWhitespace(yunshuSearch);
         if (StringUtils.isEmpty(yunshuSearch)){
-            model.addAttribute("msg", "请输入要查询的文字");
+            model.addAttribute("msg", "请输入要查询的文字或ID");
             return "yunshu/yunshu";
         }
 
+//        判断搜索内容是否为数字ID
+        if (yunshuSearch.matches("[0-9]+")){
+            Yunshu result = yunshuRepo.findById(Integer.valueOf(yunshuSearch)).orElse(null);
+            if (result == null){
+                model.addAttribute("msg", "查询不到结果");
+                return "yunshu/yunshu";
+            }
+            model.addAttribute("results", result);
+            return "yunshu/results";
+        }
+//        不是数字ID则搜索字符
+        String searchLike = "%" + yunshuSearch + "%";
+        List<Yunshu> results = yunshuRepo.findByCharacterLike((String) searchLike);
+        if (results == null){
+            model.addAttribute("msg", "查询不到结果");
+            return "yunshu/yunshu";
+        }
+        model.addAttribute("results", results);
         return "yunshu/results";
     }
 }
--- src/main/java/cn/zouhd/mandarin_corpus/repositories/YunshuRepo.java
+++ src/main/java/cn/zouhd/mandarin_corpus/repositories/YunshuRepo.java
@@ -3,6 +3,22 @@ package cn.zouhd.mandarin_corpus.repositories;
 import cn.zouhd.mandarin_corpus.entities.Yunshu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+/**
+ * The interface Yunshu repo.
+ *
+ * @author zouhd
+ * @date 2020 -04-18 18:35:11
+ */
 public interface YunshuRepo extends JpaRepository<Yunshu, Integer> {
+
+    /**
+     * 模糊查询字符
+     *
+     * @param character the character
+     * @return the list
+     */
+    List<Yunshu> findByCharacterLike(String character);
 
 }
