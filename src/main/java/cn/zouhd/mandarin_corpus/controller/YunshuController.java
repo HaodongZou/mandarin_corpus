39ebfccbd580d63416b8b7c3c53db95048ba6441
samles <13263199088@163.com>
2020-05-04 12:12:54 +0800

    添加侧边栏三级目录

--- src/main/java/cn/zouhd/mandarin_corpus/controller/YunshuController.java
+++ src/main/java/cn/zouhd/mandarin_corpus/controller/YunshuController.java
@@ -74,29 +74,29 @@ public class YunshuController {
         return "yunshu/yunshu";
     }
 
-    @GetMapping("/guangyuns")
-    public String guangyunsPage(@RequestParam Integer pageNum, Model model){
-//        Guangyun guangyunList = guangyunRepo.findById(1).orElse(null);
-        PageRequest pageRequest = PageRequest.of(pageNum - 1, 15);
-        List<Guangyun> guangyunList = guangyunRepo.findAll(pageRequest).toList();
-        model.addAttribute("results", guangyunList)
-                .addAttribute("pageNum", pageNum);
-
-        return "yunshu/guangyun";
-    }
-
-    @GetMapping("/guangyun")
-    public String guangyun(@RequestParam() String word, Model model){
-        String wordLike = "%" + word + "%";
-        List<Guangyun> byWordLike = guangyunRepo.findByWordLike(wordLike);
-        for (Guangyun guangyun : byWordLike) {
-            while(guangyun.getZhushi().contains("上同")){
-                guangyun.setZhushi(guangyunRepo.findById(guangyun.getId() - 1).orElse(null).getZhushi());
-            }
-        }
-        model.addAttribute("results", byWordLike);
-        return "searchResult";
-    }
+//    @GetMapping("/guangyuns")
+//    public String guangyunsPage(@RequestParam Integer pageNum, Model model){
+////        Guangyun guangyunList = guangyunRepo.findById(1).orElse(null);
+//        PageRequest pageRequest = PageRequest.of(pageNum - 1, 15);
+//        List<Guangyun> guangyunList = guangyunRepo.findAll(pageRequest).toList();
+//        model.addAttribute("results", guangyunList)
+//                .addAttribute("pageNum", pageNum);
+//
+//        return "zhonggu/guangyun";
+//    }
+//
+//    @GetMapping("/guangyun")
+//    public String guangyun(@RequestParam() String word, Model model){
+//        String wordLike = "%" + word + "%";
+//        List<Guangyun> byWordLike = guangyunRepo.findByWordLike(wordLike);
+//        for (Guangyun guangyun : byWordLike) {
+//            while(guangyun.getZhushi().contains("上同")){
+//                guangyun.setZhushi(guangyunRepo.findById(guangyun.getId() - 1).orElse(null).getZhushi());
+//            }
+//        }
+//        model.addAttribute("results", byWordLike);
+//        return "searchResult";
+//    }
 
 
 }
